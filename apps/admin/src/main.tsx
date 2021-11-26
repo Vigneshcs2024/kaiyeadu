b4a86066969129facb709b92b47d921d268f8eec
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-11-26 13:40:41 +0530

    added styled-components

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -4,11 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { SampleType } from '@kaiyeadu/api-interfaces';
 
 import App from './app/app';
+import styled from 'styled-components';
+
+const StyledText = styled.p`
+	font-size: 6rem;
+`;
 
 ReactDOM.render(
 	<StrictMode>
 		<BrowserRouter>
 			<App />
+			<StyledText>Hi</StyledText>
 		</BrowserRouter>
 	</StrictMode>,
 	document.getElementById('root')
