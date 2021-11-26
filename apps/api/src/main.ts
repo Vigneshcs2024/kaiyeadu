084978c1a45e83f2e2475581daa14a08895bc845
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-11-26 21:46:52 +0530

    switched from dotenv to dotenv-safe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv-safe';
+dotenv.config({ allowEmptyValues: true });
 
 import config from 'config';
 import express from 'express';
@@ -14,7 +14,7 @@ app.get('/api', (req, res) => {
 	res.send({ message: 'Welcome to api!' });
 });
 
-const port = process.env.port || config.get('api.port');
+const port = config.get('api.port') ?? process.env.PORT ?? 5000;
 const server = app.listen(port, async () => {
 	logger.info(`Listening at ${pc.cyan(`http://localhost:${port}/api`)}`);
 
