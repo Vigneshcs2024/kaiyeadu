b76b4e4bd8de26086c33edfba276e1162991da1e
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-11-26 23:07:55 +0530

    fixed some global styles

--- libs/ui/src/lib/GlobalStyles.tsx
+++ libs/ui/src/lib/GlobalStyles.tsx
@@ -1,29 +1,36 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap');
+
   * {
-    box-sizing: border-box;
+    box-sizing: inherit;
     padding: 0;
     margin: 0;
   }
-  html,
-  body {
-    font-family: "Poppins", sans-serif;
-  }
+
   html {
     font-size: 62.5%;
     scroll-behavior: smooth;
+    box-sizing: border-box;
   }
-  @media (max-width: 850px) {
+
+  body {
+    font-family: "Poppins", sans-serif;
+    font-size: 1.8rem;
+  }
+
+  @media only screen and (max-width: 850px) {
     html {
       font-size: 56.25%;
     }
   }
-  @media (max-width: 600px) {
+  @media only screen and (max-width: 600px) {
     html {
       font-size: 50%;
     }
   }
+
   :target {
     animation: background-fade 10s forwards;
     -webkit-animation: background-fade 3s forwards; /* Safari 4+ */
@@ -31,19 +38,23 @@ const GlobalStyle = createGlobalStyle`
     -o-animation:      background-fade 3s forwards; /* Opera 12+ */
     animation:         background-fade 3s forwards;
   }
+
   body::-webkit-scrollbar {
     width: 10px;
     background-color: rgba(0, 0, 0, 0.1);
     border-left: 1px solid rgba(0, 0, 0, 0.1);
   }
+
   body::-webkit-scrollbar-thumb {
     background-color: rgba(0, 0, 0, 0.4);
     border-radius: 20px;
   }
+
   a {
     color: inherit;
     text-decoration: none;
   }
+
   ul {
     padding: 0;
     margin: 0;
