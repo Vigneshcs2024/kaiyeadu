6955af7ba381fbcda6ed6c52e260481bd23611b6
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-11-30 13:56:57 +0530

    fixed auth middlewares throwing undefined error

--- apps/api/src/middlewares/auth/admin.ts
+++ apps/api/src/middlewares/auth/admin.ts
@@ -4,7 +4,7 @@ import { ClientError } from '$api/errors';
 import { ApiRequest } from '$api/types';
 
 export function adminsOnly(req: ApiRequest, _res: Response, next: NextFunction) {
-	if (req.user.role === 'admin' || req.user.role === 'master') next();
+	if (req.user?.role === 'admin' || req.user?.role === 'master') next();
 
 	throw new ClientError('You must be an admin to perform this operation', StatusCodes.FORBIDDEN);
 }
--- apps/api/src/middlewares/auth/master.ts
+++ apps/api/src/middlewares/auth/master.ts
@@ -4,7 +4,7 @@ import { ClientError } from '$api/errors';
 import { ApiRequest } from '$api/types';
 
 export function mastersOnly(req: ApiRequest, _res: Response, next: NextFunction) {
-	if (req.user.role === 'master') next();
+	if (req.user?.role === 'master') next();
 
 	throw new ClientError(
 		'You must be a super user to perform this operation',
