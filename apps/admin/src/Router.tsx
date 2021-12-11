f2b955d49f258d8d71b63ecb565ef15fb3a2916a
vishnu prakash <vishnuprakaash257@gmail.com>
2021-12-11 14:30:02 +0530

    add requests page to router

--- apps/admin/src/Router.tsx
+++ apps/admin/src/Router.tsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { AddStation, AddUser, Home, Login, Profile, Records } from './pages';
+import { AddStation, AddUser, Home, Login, Profile, Records,Requests } from './pages';
 
 export default function Router() {
 	return (
@@ -10,6 +10,7 @@ export default function Router() {
 			<Route path='/users/add' element={<AddUser />} />
 			<Route path='/police-stations/add' element={<AddStation />} />
 			<Route path='/records' element={<Records />} />
+			<Route path='/requests' element={<Requests/>} />
 
 			<Route path='*' element={<Navigate replace to='/' />} />
 		</Routes>
