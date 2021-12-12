b0c32ce7b7df26ff93f038af07a06ec76641dcff
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-12-12 12:08:31 +0530

    Moved Table component into libs

--- libs/ui/src/components/index.ts
+++ libs/ui/src/components/index.ts
@@ -2,6 +2,7 @@ export { default as TextField } from './TextField';
 export { default as TextArea } from './TextArea';
 export { default as BackgroundContainer } from './BackgroundContainer';
 export { default as Button } from './Button';
+export { default as Table } from './Table';
 export { default as Sidebar } from './Sidebar';
 export { default as SectionWithNav } from './SectionWithNav';
 export * from './DropDownList';
