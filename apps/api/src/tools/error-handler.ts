d997729b126f7097cf857627387403c0d3b349c9
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-12-14 08:49:49 +0530

    added debug logging for handled errors

--- apps/api/src/tools/error-handler.ts
+++ apps/api/src/tools/error-handler.ts
@@ -4,6 +4,7 @@ import { StatusCodes } from 'http-status-codes';
 import { UniqueConstraintError } from 'sequelize';
 import { AuthenticationError, AuthorizationError, ClientError } from '$api/errors';
 import { logger } from './logger';
+import { jsonPrettyPrint } from '$api/utilities';
 
 export const errorHandler = (
 	err: (ClientError & ValidationError) & Error,
@@ -11,13 +12,31 @@ export const errorHandler = (
 	res: Response,
 	next: NextFunction
 ) => {
+	if (
+		err instanceof ValidationError ||
+		err instanceof UniqueConstraintError ||
+		err instanceof ClientError
+	) {
+		logger.debug(
+			jsonPrettyPrint({
+				error: {
+					name: err.name,
+					message: err.message,
+					details: err.details,
+					stack: err.stack
+				}
+			})
+		);
+	}
+
 	if (err instanceof ValidationError || err.isJoi) {
 		return res.status(StatusCodes.BAD_REQUEST).json({ message: err.details[0].message });
 	}
 
 	if (err instanceof UniqueConstraintError) {
+		const field = Object.keys(err.fields)[0];
 		return res.status(StatusCodes.CONFLICT).json({
-			message: 'The particular record already exists'
+			message: `The specified ${field} is already in use`
 		});
 	}
 
