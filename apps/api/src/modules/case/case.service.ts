f07ba5da1f03aac9b7c5a306b1e99cb9f8ce4387
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-12-18 14:32:00 +0530

    added get inactive cases route

--- apps/api/src/modules/case/case.service.ts
+++ apps/api/src/modules/case/case.service.ts
@@ -13,5 +13,17 @@ export async function getAll(req: ApiRequest, res: Response) {
 
 	return res
 		.status(StatusCodes.OK)
-		.json({ message: 'Active Cases fetched successfully', result: cases });
+		.json({ message: 'Full Case details fetched successfully', result: cases });
+}
+
+export async function getInactiveCases(req: ApiRequest, res: Response) {
+	const { criminal_id } = req.params;
+
+	await Joi.string().uuid({ version: 'uuidv4' }).required().validateAsync(criminal_id);
+
+	const cases = await caseRepo.getInactiveCasesOf(criminal_id);
+
+	return res
+		.status(StatusCodes.OK)
+		.json({ message: 'Cases fetched successfully', result: cases });
 }
