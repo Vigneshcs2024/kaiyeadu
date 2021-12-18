f43f656d6c1d130eb4c36b88138b3539a8138381
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-12-18 10:49:17 +0530

    added deb setup kit

---
+++ dev_setup.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE kaiyeadu_dev;
+/* Start the server and syncronize the tables */
+INSERT INTO
+  police_stations (id, name, area, distrcit)
+VALUES
+  (
+    'b3bdd993-51e3-4e80-aaa2-8b9b8d297b30',
+    'Sample station',
+    'Sample area',
+    'Sample district'
+  );
+INSERT INTO
+  users (
+    id,
+    name,
+    email,
+    phone,
+    police_station,
+    designation,
+    role
+  )
+VALUES
+  (
+    '0ca1c4d5-72b9-4d05-991c-35dd02524796',
+    'Admin',
+    'admin@email.com',
+    '9874563210',
+    'b3bdd993-51e3-4e80-aaa2-8b9b8d297b30',
+    'Admin',
+    'Master'
+  )
+  /* Reset the admin password before use */
