3615daa7fc457e849a95aa306b59d50a317c2c05
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-12-21 12:00:41 +0530

    fixed operational-place update validation

--- apps/api/src/modules/operational-places/operational-places.service.ts
+++ apps/api/src/modules/operational-places/operational-places.service.ts
@@ -1,15 +1,16 @@
 import Joi from 'joi';
 import { Response } from 'express';
+import { OpPlaceDto } from '@kaiyeadu/api-interfaces/dtos';
 import { ApiRequest } from '$api/types';
 import * as repo from './operational-places.repository';
 import { validateOperationalPlaces } from './operational-places.validation';
 
 export async function update(req: ApiRequest, res: Response) {
 	const { id } = req.params;
-	const { body: details } = req;
+	const { body: details }: { body: OpPlaceDto } = req;
 
 	await Joi.string().uuid({ version: 'uuidv4' }).required().validateAsync(id);
-	await validateOperationalPlaces(details);
+	await validateOperationalPlaces([details]);
 
 	const updated = await repo.update(id, details);
 	res.json({ message: 'operational-places updated successfully', result: updated });
