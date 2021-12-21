665fc820a1eb9f78a27d071a4eafb11c59eac525
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-12-21 10:46:41 +0530

    added validation to family member update route

--- apps/api/src/modules/family-member/family-member.service.ts
+++ apps/api/src/modules/family-member/family-member.service.ts
@@ -1,10 +1,15 @@
+import Joi from 'joi';
 import { Response } from 'express';
 import { ApiRequest } from '$api/types';
 import * as repo from './family-member.repository';
+import { validateLinks } from '../link/link.validation';
 
 export async function update(req: ApiRequest, res: Response) {
 	const { id } = req.params;
 	const { body: details } = req;
+
+	await Joi.string().uuid({ version: 'uuidv4' }).required().validateAsync(id);
+	await validateLinks([details]);
 
 	const familyMember = await repo.update(id, details);
 
