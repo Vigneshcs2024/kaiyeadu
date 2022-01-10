38343b375e1189a74fe1c40837890e11a17e56ed
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-01-10 11:03:36 +0530

    fixed api rate limiting & cors policy

--- apps/api/src/root/setup/setup-middlewares.ts
+++ apps/api/src/root/setup/setup-middlewares.ts
@@ -6,9 +6,17 @@ import { rateLimiter, requestLogger } from '$api/middlewares';
 
 export function setup_middlewares(app: Express) {
 	app.disable('x-powered-by');
-	app.use(rateLimiter);
 	app.use(express.json());
-	app.use(cors());
+	app.use(
+		cors({
+			origin: '*',
+			methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+			allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+			exposedHeaders: ['Content-Type', 'Authorization', 'Accept']
+		})
+	);
+
+	if (config.util.getEnv('NODE_ENV') === 'production') app.use(rateLimiter);
 
 	if (config.get('logging.requests')) app.use(requestLogger);
 
