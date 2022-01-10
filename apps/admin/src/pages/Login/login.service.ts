8d261be2ceaccad261e905a4767f69a53017fec8
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-01-10 11:26:10 +0530

    added enums for requests (user, auth)

--- apps/admin/src/pages/Login/login.service.ts
+++ apps/admin/src/pages/Login/login.service.ts
@@ -1,13 +1,14 @@
 import { AxiosInstance, AxiosError } from 'axios';
 import { AdminAuthCredentialsDto } from '@kaiyeadu/api-interfaces/dtos';
 import { LoginResponse, Token } from '@kaiyeadu/api-interfaces/responses';
+import { Requests } from '@kaiyeadu/api-interfaces/constants/requests.enum';
 
 export async function login(
 	axiosInstance: AxiosInstance,
 	credentials: AdminAuthCredentialsDto
 ): Promise<Token> {
 	try {
-		const res = await axiosInstance.post<LoginResponse>('/auth/admin/login', credentials);
+		const res = await axiosInstance.post<LoginResponse>(Requests.ADMIN_LOGIN, credentials);
 		console.log(res.data.message);
 		return res.data.token;
 	} catch (error) {
