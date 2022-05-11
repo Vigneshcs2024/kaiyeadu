0ae76101af884d739746dfd3f4e850c94e4c38d3
vishnu prakash <vishnuprakaash257@gmail.com>
2022-05-11 23:32:24 +0530

    integrate crime listing api

--- apps/client/src/pages/Home/Home.tsx
+++ apps/client/src/pages/Home/Home.tsx
@@ -1,11 +1,50 @@
-import { useMemo } from 'react';
+import { useMemo, useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { BackgroundContainer, ModifyButton, Table } from '@kaiyeadu/ui/components';
-import data from './data';
+import { BackgroundContainer, ModifyButton, Table, Loader } from '@kaiyeadu/ui/components';
+import { useRequest } from '@kaiyeadu/hooks';
+import { CustomAxiosError } from '@kaiyeadu/ui/interface';
+import { Requests } from '@kaiyeadu/api-interfaces/constants/requests.enum';
 
 export default function Home() {
+	const [isLoading, setIsLoading] = useState(false);
+	const [data, setData] = useState([]);
+	const { request } = useRequest();
 	const navigate = useNavigate();
+
+	const getData = async () => {
+		setIsLoading(true);
+
+		try {
+			const res = await request.get(
+				Requests.CRIMINAL_LIST +
+					`?page=1&count=10&f={"is_goondas":false}&s={"key":"name","order":"DESC"}`
+			);
+
+			const tableValues = res.data.result.criminals.map(
+				(criminal: { dob: string; name: string; gender: string; hs_number: string }) => {
+					return {
+						first_name: criminal.name.split(' ')[0],
+						last_name: criminal.name.split(' ')[1],
+						date_of_birth: criminal.dob.substring(0, 10),
+						gender: criminal.gender,
+						hs_number: criminal.hs_number
+					};
+				}
+			);
+			setData(tableValues);
+		} catch (error) {
+			(error as CustomAxiosError).handleAxiosError?.();
+		}
+		setIsLoading(false);
+	};
+
+	const memoizedGetData = useCallback(getData, [request]);
+
+	useEffect(() => {
+		memoizedGetData();
+	}, [memoizedGetData]);
+
 	const columns = useMemo(
 		() => [
 			{
@@ -40,6 +79,7 @@ export default function Home() {
 		<BackgroundContainer
 			style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
 			pageTitle='Home'>
+			{isLoading && <Loader withOverlay={false} />}
 			<Table columns={columns} data={data} navigateTo={navigateToDetails} />
 		</BackgroundContainer>
 	);
