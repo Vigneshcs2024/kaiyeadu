7da00831ae5ea2f73c02efdd68b2924b6c14fce5
PranavGPR <pranav.gnanasekar23@gmail.com>
2022-05-29 17:12:39 +0530

    Added function for bulk user insertion

--- migrations/index.js
+++ migrations/index.js
@@ -1,13 +1,27 @@
 const axios = require('axios');
 
-const file = require('./sampleData.json');
+const criminalFile = require('./sampleData.json');
+const userFile = require('./userData.json');
 
 const token =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjBjYTFjNGQ1LTcyYjktNGQwNS05OTFjLTM1ZGQwMjUyNDc5NiIsIm5hbWUiOiJBZG1pbiIsImRlc2lnbmF0aW9uIjoiU0kiLCJyb2xlIjoibWFzdGVyIiwiaWF0IjoxNjUyOTM3MjU2LCJleHAiOjE2NTMwMjM2NTYsImlzcyI6IlROUE9MIn0.y1or6-s1MXpzKXTtJ57Brcvh5S1e7QS4LwQ90gTBgcQ';
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjBjYTFjNGQ1LTcyYjktNGQwNS05OTFjLTM1ZGQwMjUyNDc5NiIsIm5hbWUiOiJBZG1pbiIsImRlc2lnbmF0aW9uIjoiU0kiLCJyb2xlIjoibWFzdGVyIiwiaWF0IjoxNjUzODIyMDEzLCJleHAiOjE2NTM5MDg0MTMsImlzcyI6IlROUE9MIn0.OsAE3l78wA05ySfQY51gjIrWW1SWDfTVg51Z8nRMajY';
 
-file.arrayOfJson.forEach(async element => {
+criminalFile.arrayOfJson.forEach(async element => {
 	try {
 		const res = await axios.post('http://localhost:5000/criminal/create', element, {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		});
+		console.log(res.data.message);
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+userFile.arrayOfJson.forEach(async element => {
+	try {
+		const res = await axios.post('http://localhost:5000/user/create', element, {
 			headers: {
 				Authorization: `Bearer ${token}`
 			}
