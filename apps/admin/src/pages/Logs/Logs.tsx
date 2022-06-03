fd9a11c48872c48d086190a58c1729d08d7aadb7
PranavGPR <pranav.gnanasekar23@gmail.com>
2022-06-03 21:35:05 +0530

    Integrated Logs API

--- apps/admin/src/pages/Logs/Logs.tsx
+++ apps/admin/src/pages/Logs/Logs.tsx
@@ -1,32 +1,106 @@
-import { useMemo } from 'react';
-import styled from 'styled-components';
+import { useCallback, useLayoutEffect, useMemo, useState } from 'react';
 
-import { BackgroundContainer, Table } from '@kaiyeadu/ui/components';
-import data from './data';
+import { BackgroundContainer, Loader, Pagination, Searchbar, Table } from '@kaiyeadu/ui/components';
 import { Layout } from '@kaiyeadu/ui/styles';
+import { CustomAxiosError } from '@kaiyeadu/ui/interface';
+import { useRequest } from '@kaiyeadu/hooks';
+import { Requests } from '@kaiyeadu/api-interfaces/constants/requests.enum';
+import toast from 'react-hot-toast';
+import { recordCount } from '@kaiyeadu/api-interfaces/constants';
+import { SortBy } from '@kaiyeadu/ui/components/Filter/SortBy';
+
+interface LogResponse {
+	id: string;
+	log: string;
+	createdAt: string;
+	user_id: {
+		name: string;
+	};
+	criminal_id: {
+		name: string;
+	};
+	police_station_id: {
+		name: string;
+	};
+}
 
 export default function Logs() {
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [page, setPage] = useState(1);
+	const [totalPages, setTotalPages] = useState(1);
+	const [search, setSearch] = useState('');
+
+	const { request } = useRequest();
+
+	const getData = async () => {
+		setTimeout(async () => {
+			try {
+				setLoading(true);
+				const res = await request.get(
+					Requests.LOGS_LIST +
+						`?page=${page}&count=${recordCount}` +
+						(search ? `&q=${search}` : '')
+				);
+
+				let totalPagesCalc = Math.round(res.data.result.total / recordCount);
+
+				if (totalPagesCalc < 1) {
+					console.log(totalPagesCalc);
+					totalPagesCalc = 1;
+				} else {
+					setTotalPages(totalPagesCalc);
+				}
+
+				const tableValues = res.data.result.logs.map((item: LogResponse) => {
+					return {
+						id: item.id,
+						log: item.log,
+						createdAt: new Date(item.createdAt).toLocaleString(),
+						user_name: item.user_id.name,
+						criminal_name: item.criminal_id ? item.criminal_id.name : null,
+						police_station_name: item.police_station_id
+							? item.police_station_id.name
+							: null
+					};
+				});
+				toast.success(res.data.message);
+				setData(tableValues);
+				setLoading(false);
+			} catch (error) {
+				(error as CustomAxiosError).handleAxiosError?.();
+			}
+		}, 500);
+	};
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const memoizedGetData = useCallback(getData, [page, request]);
+
+	useLayoutEffect(() => {
+		memoizedGetData();
+	}, [memoizedGetData]);
+
 	const columns = useMemo(
 		() => [
 			{
-				Header: 'First Name',
-				accessor: 'first_name'
+				Header: 'User Name',
+				accessor: 'user_name'
 			},
 			{
-				Header: 'Last Name',
-				accessor: 'last_name'
+				Header: 'Log Message',
+				accessor: 'log'
 			},
 			{
-				Header: 'Gender',
-				accessor: 'gender'
+				Header: 'Criminal Name',
+				accessor: 'criminal_name'
 			},
 			{
-				Header: 'HS Number',
-				accessor: 'hs_number'
+				Header: 'Police Station Name',
+				accessor: 'police_station_name'
 			},
 			{
-				Header: 'Date of Birth',
-				accessor: 'date_of_birth'
+				Header: 'Created At',
+				accessor: 'createdAt'
 			}
 		],
 		[]
@@ -35,6 +109,9 @@ export default function Logs() {
 	return (
 		<BackgroundContainer pageTitle='Admins'>
 			<Layout>
+				{loading && <Loader withOverlay={true} />}
+				<Searchbar setSearch={setSearch} getData={getData} />
+				<Pagination page={page} setPage={setPage} totalPages={totalPages} />
 				<Table columns={columns} data={data} />
 			</Layout>
 		</BackgroundContainer>
